26767111367af38708e03aaebdd3c628e63e5727
syahrulmf <50761419+syahrulmf@users.noreply.github.com>
2021-09-09 13:43:34 +0700

    add number function

---
+++ day-1/number.js
@@ -0,0 +1,28 @@
+// -----------------------------------------------------  //
+// ----- Alterra Academy - JAVASCRIPT 101 Workshop -----  //
+// -----------------------------------------------------  //
+
+// -------------------- Number -------------------------  //
+
+// no error
+let message = "Hello";
+message = 12345;
+
+console.log(message) //will log 12345
+
+let n = 123;
+n = 123.456;
+let integer = 4;
+let float = 4.7;
+let max = +Infinity;
+
+// some operations with numbers
+let sum = 4 + 5; // adding two numbers
+let difference = 9 - 4; // subtracting two number
+let product = 4 * 5; // multiplying two number
+let quotient = 20 / 5; // dividing two number
+let remainder = 9 % 2; // modulop
+
+console.log(1 / 0) // infinity
+console.log(Infinity) // infinity
+console.log("not a number" / 2) // Nan, such divission is erroneous
