8126dde4bf9d8656d16d0fbf35b186a85fb7f929
syahrulmf <50761419+syahrulmf@users.noreply.github.com>
2021-09-09 13:56:22 +0700

    add operator function

---
+++ day-1/operator.js
@@ -0,0 +1,31 @@
+// -----------------------------------------------------  //
+// ----- Alterra Academy - JAVASCRIPT 101 Workshop -----  //
+// -----------------------------------------------------  //
+
+// --------------- Arithmetic Operator -----------------  //
+
+let a, b, add, sub, mul, div, mod;
+
+a = 10;
+b = 20;
+
+add = a + b;
+console.log("The result is " + add)
+sub = a - b;
+console.log("The result is " + sub)
+mul = a * b;
+console.log("The result is " + mul)
+div = a / b;
+console.log("The result is " + div)
+mod = a % b;
+console.log("The result is " + mod)
+
+// Increment
+let increment = 5;
+increment++
+console.log(increment) // will log 6
+
+// Decrement
+let decrement = 5;
+decrement--
+console.log(decrement) // will log 4
